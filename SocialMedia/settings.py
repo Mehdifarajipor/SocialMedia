d4349af736196eccf6b4d49fbed3da014048df89
mehdi <mehdifarajise666@gmail.com>
2025-09-05 10:51:58 +0330

    database changed

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'SocialMedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SocialMedia',
+        'USER': 'SocialMediaUser',
+        'PASSWORD': 'xcxXbX@fr$',
+        'PORT': '5432',
     }
 }
 
